4baca2f0e27a4814fef55619752897b52ebfd7ab
sauljimenez24 <a.22308051281222@cbtis128.edu.mx>
2024-09-20 09:31:40 -0600

    Add files via upload

---
+++ operadoresA.py
@@ -0,0 +1,55 @@
+print("Act 9 clases v2")
+print("saul jimenez mat: 22308051281222 ")
+# Zona de clases
+
+class Operadores1222:
+    # zona de funciones
+    def suma1222(self,E,N):
+        s1222=E+N
+        print(f" la suma de {E} + {N} = {s1222}")
+
+    def resta1222(self,R,M):
+        a1222=R-M
+        print(f" la resta de {R} - {M} = {a1222}")
+
+    def multiplicacion1222(self,b,y):
+        j1222=b*y
+        print(f" la multiplicacion de {b} * {y} = {j1222}")
+
+    def divicion1222(self,s,r):
+        r1222=s/r
+        print(f" la divicion de {s} / {r} = {r1222}")
+
+    def modulo1222(self,w,x):
+        x1222=w%x
+        print(f" el modulo de {w} % {x} = {x1222}")
+
+    def exponente1222(self,k,l):
+        e1222=k**l
+        print(f" la exponente  de {k} ** {l} = {e1222}")
+    def cociente1222(self,t,h):
+        h1222=t//h
+        print(f" la cociente de {t} // {h} = {h1222}")
+
+
+
+# zona creacion de objeto 
+operasaul=Operadores1222()
+
+# zona de uso de objetos
+print("funcion suma ")
+operasaul.suma1222(5,4)
+
+print("funcion resta ")
+operasaul.resta1222(8,3)
+
+print("funcion multiplicacion ")
+operasaul.multiplicacion1222(5,5)
+
+print("funcion divicion ")
+operasaul.divicion1222(67,34)
+
+print("funcion modulo ")
+operasaul.modulo1222(9,21)
+print("funcion cociente ")
+operasaul.cociente1222(10,75)
